372db23d6b369b510dbafcb23aaa303636ef1936
read-jord <redajord19123@gmail.com>
2023-03-21 15:37:15 -0700

    nn

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: single letter input
  * Return: 1 if int c is lowercase, 0 if otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
 	{
