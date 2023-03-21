4ab9a2392d739541e47f1dbdbcb120cf944dd7f3
read-jord <redajord19123@gmail.com>
2023-03-21 14:10:08 -0700

    r

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+int main (void){
+
+void print_alphabet(void){
+char a ='a';
+while(a<='z'){
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
+
+return(0);
+}
