a3336d978f7bfe06ff904a9c97909cd36006b80c
read-jord <redajord19123@gmail.com>
2023-03-21 07:56:59 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,16 @@
-#include "Jord.h"
+#include "main.h"
 
-
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        _putchar('j');
+	_putchar('j');
+	_putchar('o');
+	_putchar('r');
+	_putchar('d');
 	_putchar('\n');
-	return(0);
-	
+
+	return (0);
 }
