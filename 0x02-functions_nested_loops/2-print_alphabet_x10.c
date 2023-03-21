8a6b6daa59edc34e0204d11b071429647c332531
read-jord <redajord19123@gmail.com>
2023-03-21 14:29:31 -0700

    he

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i=0; i<10; i++) 
+{
+	char n = 'a';
+	while (n <= 'z')
+	{
+		_putchar(c);
+		n++;
+	
+	}
+}
+}
