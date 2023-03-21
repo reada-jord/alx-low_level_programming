8f348ff934e809127511ffc6e6c4fd6b6fa92156
reada-jord <redajord19123.com>
2023-03-21 07:17:13 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,10 @@
-#include<stdio.h>
-void putchar(char c) {
-  /*
-   * Correct answer to this question == Code that implements putchar(char).
-   * Please note: no #include:s allowed. Not even a single one :-)
-   */
-}
+#include "main.h"
+
 
 int main(void)
 {
-        putchar('j');
-	putchar('\n');
+        _putchar('j');
+	_putchar('\n');
 	return(0);
 	
 }
